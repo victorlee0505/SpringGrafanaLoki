0119738adf4747cff8972661b31734480bdc530f
Victor Lee <victorlee0505@gmail.com>
2021-02-18 15:43:43 -0500

    Main: log APP_STARTED

--- src/main/java/com/example/monitoring/demo/DemoApplication.java
+++ src/main/java/com/example/monitoring/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.example.monitoring.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		LOG.info("APP_STARTED");
 	}
 
 }
